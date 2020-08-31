df82c112cd73e55df66d4849fda70b4c01812972
Kiril Vatev <vatev.1@gmail.com>
2020-08-31 18:26:23 -0400

    using puppeteer for run tests

--- test/smoke.test.js
+++ test/smoke.test.js
@@ -1,9 +1,6 @@
 const { expect } = require('chai');
 
-const {
-  start, stop,
-  waitForVisible, waitForElementCount
-} = require('./lib/app-provider.js');
+const { start, stop } = require('./lib/app-provider-puppeteer.js');
 const config = require('./lib/config-provider.js');
 
 describe('[smoke tests]', () => {
@@ -35,24 +32,24 @@ describe('[smoke tests]', () => {
     const configPath = await config.create({});
     const app = await start(configPath);
 
-    await waitForVisible('#app');
-    await waitForElementCount('p', 1);
+    await app.waitForVisible('#app');
+    await app.waitForElementCount('p', 1);
 
-    expect(await app.legacy.getText('#app p')).to.include('This is your app');
+    expect(await app.getText('#app p')).to.include('This is your app');
   });
 
   it('counts when clicking the button', async () => {
     const configPath = await config.create({});
     const app = await start(configPath);
 
-    await waitForVisible('.app button');
+    await app.waitForVisible('.app button');
 
     // maybe consider using better selectors, but you get the idea
-    expect(await app.legacy.getText('.app > div > span')).to.equal('0');
+    expect(await app.getText('.app > div > span')).to.equal('0');
 
-    await app.legacy.click('.app button');
+    await app.click('.app button');
 
-    expect(await app.legacy.getText('.app > div > span')).to.equal('1');
+    expect(await app.getText('.app > div > span')).to.equal('1');
   });
 
   it('loads the previously counted value', async () => {
@@ -62,12 +59,12 @@ describe('[smoke tests]', () => {
     });
     const app = await start(configPath);
 
-    await waitForVisible('#app button');
+    await app.waitForVisible('#app button');
 
-    expect(await app.legacy.getText('.app > div > span')).to.equal('72');
+    expect(await app.getText('.app > div > span')).to.equal('72');
 
-    await app.legacy.click('.app button');
+    await app.click('.app button');
 
-    expect(await app.legacy.getText('.app > div > span')).to.equal('73');
+    expect(await app.getText('.app > div > span')).to.equal('73');
   });
 });
