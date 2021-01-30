71a34fff01621b1f6e679a16916e0ff3bf5ffc48
Kiril Vatev <vatev.1@gmail.com>
2021-01-30 13:49:29 -0500

    removing "setup" from the macos name

--- .electron-builder.js
+++ .electron-builder.js
@@ -21,7 +21,7 @@ module.exports = {
   },
   dmg: {
     icon,
-    artifactName: `${fileName}-v\${version}-MacOS-setup.\${ext}`
+    artifactName: `${fileName}-v\${version}-MacOS.\${ext}`
   },
   win: {
     icon,
